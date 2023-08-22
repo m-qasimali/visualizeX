2d6c99c8d7ebd5a5d6d0e490da2b406b33b70928
m-qasim-ali <muhammadqasimali96@gmail.com>
2023-08-22 08:15:53 +0500

    Team updated as team folder import

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
-import { ColorModeContext, useMode } from './theme.js';
-import { CssBaseline, Stack, ThemeProvider } from '@mui/material';
-import Topbar from './scenes/global/Topbar.jsx';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { ConfirmProvider } from 'material-ui-confirm';
+import React from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Dashboard from './scenes/dashboard/index.jsx';
-import Team from './scenes/Team/index.jsx';
-import Invoices from './scenes/Invoices/index.jsx';
-import Contacts from './scenes/contacts/index.jsx';
+import Home from './scenes/Home.jsx';
 import Bar from './scenes/bar/index.jsx';
+import Calendar from './scenes/calendar/index.jsx';
+import Contacts from './scenes/contacts/index.jsx';
+import Dashboard from './scenes/dashboard/index.jsx';
+import FAQ from './scenes/faq/index.jsx';
 import Form from './scenes/form/index.jsx';
+import Geography from './scenes/geography/index.jsx';
+import Invoices from './scenes/invoices/index.jsx';
 import Line from './scenes/line/index.jsx';
 import Pie from './scenes/pie/index.jsx';
-import FAQ from './scenes/faq/index.jsx';
-import Geography from './scenes/geography/index.jsx';
-import Calendar from './scenes/calendar/index.jsx';
-import Sidebar from './scenes/global/Sidebar.jsx';
-import Home from './scenes/Home.jsx';
-import { ConfirmProvider } from 'material-ui-confirm';
+import Team from './scenes/team/index.jsx';
+import { ColorModeContext, useMode } from './theme.js';
 
 const router = createBrowserRouter([
   {
