7e0b49dc2ba942a9d66b7347152ed240c770606a
m-qasim-ali <muhammadqasimali96@gmail.com>
2023-08-22 08:10:37 +0500

    vite.config updated

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ config }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = 'visualizeX'
-  }
-
-  return config;
+export default defineConfig({
+  plugins: [react()],
+  base: '/visualizeX'
 })
